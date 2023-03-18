7b26e38db7745c28a199af319b9f9725930d6674
furffico <jerrywang0206@gmail.com>
2023-03-18 19:26:23 +0800

    optimize cvrp insertion

--- utils/insertion/__init__.py
+++ utils/insertion/__init__.py
@@ -30,17 +30,10 @@ def random_insertion(cities, order):
 
     return result, cost
 
-def cvrp_random_insertion(customerpos, depotpos, demands, capacity, order = None, exploration = 0.7):
+def cvrp_random_insertion(customerpos, depotpos, demands, capacity, order = None, exploration = 1.0):
     assert len(customerpos.shape) == 2 and customerpos.shape[1] == 2
     assert isinstance(capacity, int)
 
-    ccount = customerpos.shape[0]
-    if order is None:
-        order = np.arange(ccount, dtype=np.uint32)
-    else:
-        assert len(order.shape) == 1 and order.shape[0] == ccount
-        order = _to_numpy(order).astype(np.uint32)
-    
     if isinstance(depotpos, tuple):
         assert len(depotpos)==2
         depotx, depoty = depotpos
@@ -49,21 +42,33 @@ def cvrp_random_insertion(customerpos, depotpos, demands, capacity, order = None
         depotx, depoty = depotpos[0].item(), depotpos[1].item()
     depotx, depoty = float(depotx), float(depoty)
 
+    ccount = customerpos.shape[0]
+    if order is None:
+        # generate order
+        dx, dy = (customerpos - np.array([[depotx, depoty]])).T
+        phi = np.arctan2(dy, dx)
+        order = np.argsort(phi).astype(np.uint32)
+    else:
+        assert len(order.shape) == 1 and order.shape[0] == ccount
+        order = _to_numpy(order).astype(np.uint32)
+
     customerpos = _to_numpy(customerpos)
     if customerpos.dtype is not np.float32:
         customerpos = customerpos.astype(np.float32)
     demands = _to_numpy(demands)
     if demands.dtype is not np.uint32:
         demands = demands.astype(np.uint32)
-    order, sep = insertion.cvrp_random(customerpos, depotx, depoty, demands, capacity, order, exploration)
-    routes = [order[i:j] for i,j in zip(sep, sep[1:])]
+    
+    outorder, sep = insertion.cvrp_random(customerpos, depotx, depoty, demands, capacity, order, exploration)
+    routes = [outorder[i:j] for i,j in zip(sep, sep[1:])]
     return routes
 
-def cvrplib_random_insertion(positions, demands, capacity, order = None, exploration = 0.7):
+def cvrplib_random_insertion(positions, demands, capacity, order = None, exploration = 1.0):
     customerpos = positions[1:]
     depotpos = positions[0]
     demands = demands[1:]
-    order = np.delete(order, order==0) - 1
+    if order is not None:
+        order = np.delete(order, order==0) - 1
     routes = cvrp_random_insertion(customerpos, depotpos, demands, capacity, order, exploration)
     for r in routes:
         r += 1
@@ -71,10 +76,9 @@ def cvrplib_random_insertion(positions, demands, capacity, order = None, explora
 
 
 if __name__=="__main__":
-    n = 51
+    n = 100
     pos = np.random.rand(n, 2)
     depotpos = pos.mean(axis=0)
     demands = np.random.randint(1, 7, size = n)
     capacity = 30
-    order=np.arange(0, n, dtype=np.uint32)
-    print(*cvrplib_random_insertion(pos, demands, capacity, order), sep='\n')
+    print(*cvrp_random_insertion(pos, depotpos, demands, capacity), sep='\n')
